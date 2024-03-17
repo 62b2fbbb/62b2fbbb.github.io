369b24cb69d308d6b63193ba1fa2269f5d078930
62b2fbbb <162927309+62b2fbbb@users.noreply.github.com>
2024-03-17 21:07:31 +0200

    Update buybutton.js

--- buybutton.js
+++ buybutton.js
@@ -2,11 +2,18 @@ let buy = document.getElementById("buybtn");
 let header = document.getElementById("header");
 let main = document.getElementById("main");
 let mainmenu = document.getElementById("mainmenu");
+let body = document.getElementById("body");
 buy.addEventListener("click", function(){
-	main.style.backgroundColor = "red";
-	main.style.filter = "blur(25px)";
+	// main.style.backgroundColor = "red";
+	main.style.filter = "blur(50px)";
 	header.style.zIndex = "7";
 	header.style.opacity = "70%";
 	mainmenu.classList.add("menu");
 	mainmenu.classList.remove("menuhidden");
+	main.addEventListener("click", function(){
+		main.style.filter = "none";
+		mainmenu.classList.add("menuhidden");
+		mainmenu.classList.remove("menu");
+		main.removeEventListener
+	})
 })
